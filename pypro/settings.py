240a598d02a7a897bdacaca5ee54f3387be8b516
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-03-13 10:43:56 -0300

    Criados arquivos para deploy no Heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c!%-sx1dcnc4%2n!q#xsw8qohg+**!jj70+i@7i4mh@gu#*alt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
