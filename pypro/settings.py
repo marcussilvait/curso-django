c6b74064fbbed48c6e9b010ad5a07c86f10f875f
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-03-15 15:18:12 -0300

    Instalada lin python-decouple e extraída configuração DEBUG

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c!%-sx1dcnc4%2n!q#xsw8qohg+**!jj70+i@7i4mh@gu#*alt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
