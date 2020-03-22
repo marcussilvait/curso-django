60b9267bb5a50ea1ba4e78a488bd01911ec893d9
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-03-22 12:44:25 -0300

    Removida configuração collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -127,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-COLLECTFAST_ENABLED = False
+# COLLECTFAST_ENABLED = False
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
@@ -142,7 +141,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
 
-    COLLECTFAST_ENABLED = True
+#    COLLECTFAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'private'
 
