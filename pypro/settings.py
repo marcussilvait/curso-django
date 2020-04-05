28ca69559f58a0df6f830fc8f5777fed2494547e
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-04-05 19:51:28 -0300

    Configurado Template Tag Static

--- pypro/settings.py
+++ pypro/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 
 INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
 
-# if DEBUG:
-#     INSTALLED_APPS.append('debug_toolbar')
-#     MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
